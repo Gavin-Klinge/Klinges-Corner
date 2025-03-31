ee5c0150d12758fe1a1b99a0cad391b6d587d98a
Gavin-Klinge <gavin.klinge@gmail.com>
2025-03-31 14:33:32 -0500

    test2

--- js/header.js
+++ js/header.js
@@ -34,10 +34,12 @@ function initMobileMenu() {
 // Handle scroll events
 function handleScroll() {
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
+    if (navbar) {
+        if (window.scrollY > 0) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
     }
 }
 
